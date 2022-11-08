865fc1e02be96f39b9f9162dbdc8aed5e63579f6
NaN-Simon <goosaimon@gmail.com>
2022-11-08 18:41:00 +0300

    feat: add answer after entered, JSON instead of array

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable prefer-const */
 import './styles/_index.scss';
+// import * as questions3 from './questions.json';
+
+const questions: Quest[] = require('./questions.json');
+
 // const test: HTMLElementTagNameMap
 // this === HTMLElement
 
@@ -10,27 +14,10 @@ interface Quest {
   answer: string[];
   right: string;
 }
-const questions: Quest[] = [
-  {
-    quest: 'chair',
-    answer: ['стол', 'стул', 'кресло', 'диван'],
-    right: 'стул',
-  },
-  {
-    quest: 'couch',
-    answer: ['стол', 'стул', 'кресло', 'диван'],
-    right: 'диван',
-  },
-  {
-    quest: 'table',
-    answer: ['стол', 'стул', 'кресло', 'диван'],
-    right: 'стол',
-  },
-];
 
+const supCard = document.querySelector('.sup-card') as HTMLElement;
 const card = document.querySelector('.card') as HTMLElement;
 const btn = document.querySelector('.button') as HTMLButtonElement;
-const form = document.forms[0] as HTMLFormElement;
 
 let questIndex = 0;
 let totalScore = 0;
@@ -38,37 +25,29 @@ let mistalesArrow: string[] = [];
 
 btn?.addEventListener('click', btnClickHandler);
 
-displayNewQuestion();
+card.innerHTML = displayNewQuestion();
 
 function btnClickHandler() {
   const answer = getUserAnswer();
   const rightAnswer = questions[questIndex].right;
   const result = answer === rightAnswer;
-  if (answer === undefined) {
-    alert('Choose answer');
+  if (answer === undefined) return;
+  displayAnswerStatus(result);
+  addScorePointOrPushMistake(answer, rightAnswer);
+  questIndex++;
+  if (questions.length !== questIndex) {
+    card.innerHTML = displayNewQuestion();
   } else {
-    displayAnswer(result);
-    addScorePointOrPushMistake(answer, rightAnswer);
-    questIndex++;
-    displayNewQuestion();
+    questIndex = 0;
+    card.innerHTML = endgame();
   }
 }
 
-function addScorePointOrPushMistake(answer: string, rightAnswer: string): void {
+function addScorePointOrPushMistake(answer?: string, rightAnswer?: string): void {
   if (answer === rightAnswer) {
     totalScore++;
   } else {
-    mistalesArrow.push(`Правильный ответ: ${answer} - Твой ответ: ${rightAnswer}`);
-  }
-}
-
-function displayAnswer(result: boolean) {
-  if (result) {
-    alert('Верно!');
-    // totalScore++;
-  } else {
-    alert('Неверно!');
-    // mistalesArrow.push(`${value} - ${rightAnswer}`);
+    mistalesArrow.push(`${answer} - ${rightAnswer}`);
   }
 }
 
@@ -78,25 +57,50 @@ function getUserAnswer() {
   return checkedTextAnswer;
 }
 
-function displayNewQuestion(): void {
-  if (questions.length !== questIndex) {
-    let liHTML = questions[questIndex].answer.map((singleAnswer) => `
-      <li class="card__answer-item">
-        <label class="card__answer-item-label">
-        <input type="radio" name="answer">
-        <span>${singleAnswer}</span></label>
-      </li>
-      `);
+function displayNewQuestion() {
+  let liHTML = questions[questIndex].answer.map((singleAnswer) => `
+    <li class="card__answer-item">
+      <label class="card__answer-item-label">
+      <input type="radio" name="answer">
+      <span>${singleAnswer}</span></label>
+    </li>
+    `);
 
-    card.innerHTML = `
-      <div class="question">Какой перевод слова(фразы) 
-        <strong>${questions[questIndex].quest}</strong>?
-      </div>
-      <ul class="card__answer">
-        ${liHTML.join('')}
-      </ul>`;
+  return `
+    <div class="question">Какой перевод слова(фразы) 
+      <strong class="word">${questions[questIndex].quest}</strong>?
+    </div>
+    <ul class="card__answer">
+      ${liHTML.join('')}
+    </ul>`;
+}
+
+function displayAnswerStatus(result: boolean):void {
+  if (result) {
+    supCard.innerHTML = 'Верно!';
   } else {
-    questIndex = 0;
-    card.innerHTML = 'Game over!';
+    supCard.innerHTML = 'Неверно!';
   }
+  setTimeout(() => {
+    supCard.innerHTML = '';
+  }, 3000);
+}
+
+function endgame() {
+  const resultArray = mistalesArrow.map((item) => `<li class="card__answer-item">${item}</li>`).join('');
+  let scoreList = '';
+  if (mistalesArrow.length !== 0) {
+    scoreList = `
+    <h5>Правильный ответ - неправильный ответ</h5>
+    <ul>${resultArray}<ul>
+    `;
+  } else {
+    scoreList = 'Все верно!';
+  }
+
+  return `
+  <h1>Game over!</h1>
+  <h2>Балл: ${totalScore}</h2>
+  ${scoreList}
+  `;
 }
